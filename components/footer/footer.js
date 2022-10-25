fcd84b6458b9ef8f04dfb5c9ffdcf3dfcd302cab
Amin <aminfarid1234@gmail.com>
2022-10-25 18:03:31 +0330

    adding style footer

--- components/footer/footer.js
+++ components/footer/footer.js
@@ -1,11 +1,95 @@
 import classes from "./footer.module.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faInstagram,
+  faFacebook,
+  faTwitter,
+  faPinterest,
+} from "@fortawesome/free-brands-svg-icons";
+import {
+  faPhone,
+  faEnvelope,
+  faMapMarker,
+} from "@fortawesome/free-solid-svg-icons";
+import Link from "next/link";
 
 const Footer = () => {
-    return(
-        <div className="h-screen">
-            Footer
+  return (
+    <div className=" bg-header px-8 py-8 flex justify-between text-white md:flex-col md:gap-10">
+      <div className="flex flex-col gap-6">
+        <h1 className="text-2xl">Our social Media</h1>
+        <div className="flex gap-4">
+          <div
+            className={classes.socialMedia}
+            style={{ background: "#3B5999" }}
+          >
+            <FontAwesomeIcon className="text-xl" icon={faFacebook} />
+          </div>
+          <div
+            className={classes.socialMedia}
+            style={{ background: "#E4405F" }}
+          >
+            <FontAwesomeIcon icon={faInstagram} />
+          </div>
+          <div
+            className={classes.socialMedia}
+            style={{ background: "#55ACEE" }}
+          >
+            <FontAwesomeIcon icon={faTwitter} />
+          </div>
+          <div
+            className={classes.socialMedia}
+            style={{ background: "#E60023" }}
+          >
+            <FontAwesomeIcon icon={faPinterest} />
+          </div>
         </div>
-    )
-}
+      </div>
+
+      <div className="flex flex-col gap-6">
+        <h1 className="text-2xl">Useful Links</h1>
+        <div className="flex gap-10">
+          <ul className="flex flex-col gap-3">
+            <li>
+              <Link href="/">Home</Link>
+            </li>
+            <li>
+              <Link href="/">Man Fashion</Link>
+            </li>
+            <li>
+              <Link href="/">Accessories</Link>
+            </li>
+          </ul>
+          <ul  className="flex flex-col gap-3">
+            <li>
+              <Link href="/">Older Tracking</Link>
+            </li>
+            <li>
+              <Link href="/">Wishlist</Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+
+      <div className="flex flex-col gap-6">
+        <h1 className="text-2xl">Contact</h1>
+        <ul className="flex flex-col gap-4">
+          <li className="flex gap-2">
+            <FontAwesomeIcon className="pt-1" color="#FF4A59" icon={faMapMarker} />
+            <p>622 Dixie, South Tobinchester 98336</p>
+          </li>
+          <li className="flex gap-2">
+            <FontAwesomeIcon className="pt-1" color="#FF4A59" icon={faPhone} />
+            <p>+ 1 234 56 78</p>
+          </li>
+          <li className="flex gap-2">
+            <FontAwesomeIcon className="pt-1" color="#FF4A59" icon={faEnvelope} />
+            <p>aminfarid1234@gmail.com</p>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
 
 export default Footer;
