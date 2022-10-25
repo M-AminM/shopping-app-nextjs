18e6547197e2010ff26332f0c98c7465375e4134
Amin <aminfarid1234@gmail.com>
2022-10-25 18:06:57 +0330

    Adding footer to all pages

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,12 @@
 import Header from "../components/header/header";
 import "../styles/globals.css";
+import Footer from "../components/footer/footer";
 
 function MyApp({ Component, pageProps }) {
   return (
     <Header>
       <Component {...pageProps} />
+      <Footer />
     </Header>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,7 @@
-import styles from "../styles/Home.module.css";
 import Slider from "../components/slider/slider";
 import { Fragment } from "react";
 import Products from "../components/products/products";
 import Newsletter from "../components/newsletter/newsletter";
-import Footer from "../components/footer/footer";
 
 export default function Home() {
   return (
@@ -11,7 +9,7 @@ export default function Home() {
       <Slider />
       <Products />
       <Newsletter />
-      <Footer />
+      
     </Fragment>
   );
 }
